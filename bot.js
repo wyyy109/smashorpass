07b6edeb4ead0945e3e300d1ac9b7ffaff2ed27e
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2019-11-05 14:23:04 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,33 +13,25 @@ bot.on('message', function(message){
     {
         message.channel.send({files: ["https://i.imgur.com/jiaoi7A.gif"]});
     }
-});
-
-bot.on('message', function(message){
-    if(message.content == 'f')
+    else if(message.content == 'f')
     {
         message.channel.send({files: ["https://i.imgur.com/jiaoi7A.gif"]});
     }
-});
-
-bot.on('message', function(message){
-    if(message.content == 'F')
+    else if(message.content == 'F')
     {
         message.channel.send({files: ["https://i.imgur.com/jiaoi7A.gif"]});
     }
-});
-
-bot.on('message', function(message){
-    if(message.content == '!gay')
+    else if(message.content == '!gay')
     {
         message.channel.send({files: ["https://i.imgur.com/MOLAAtG.png"]});
     }
-});
-
-bot.on('message', function(message){
-    if(message.content == '!ocelot')
+    else if(message.content == '!ocelot')
     {
         message.channel.send({files: ["https://i.imgur.com/W8x06md.gif"]});
+    }
+    else if(message.content == '!balloon')
+    {
+        message.channel.send({files: ["https://i.imgur.com/j3t3Sgv.gif"]});
     }
 });
 
