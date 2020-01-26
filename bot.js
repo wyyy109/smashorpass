334969904936c44545522a2e18b74d72f4899e19
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-25 22:31:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-//code goes here
+bot.on('message', function(message){
+    if(message.content.toLowerCase().startsWith('smash or pass:'))
+    {
+        message.channel.send(
+            "The vote begins! /n**Name**: smash or pass? Select :heart: to smash, :skull: to pass."
+            ).react(❤️).react(💀);
+    }
+});
 
 bot.login(process.env.BOT_TOKEN);
