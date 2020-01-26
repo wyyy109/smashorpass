b630602bf91c51e91319a00512a905ad192b3004
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-25 23:44:06 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,16 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-bot.on('message', async function(message){
+bot.on('message', function(message){
     if(message.content.toLowerCase().startsWith('smash or pass:'))
     {
         message.channel.send(
             "The vote begins! \n**Name**: smash or pass? Select :heart: to smash, :skull: to pass.")
-            try {
-                await message.react("❤️")
-                await message.react("💀")
-            }
-            catch (error) {
-                console.error('One of the emojis failed to react.');
-            }
+                .then(async function (message){
+                    try {
+                    await message.react("❤️")
+                    await message.react("💀")
+                    }
+                catch (error) {
+                    console.error('One of the emojis failed to react.');
+                    }
+                })
     }
 });
-
-bot.login(process.env.BOT_TOKEN);
