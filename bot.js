f20f1a06d4447210a5624aa47bedb852debe5bab
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-26 21:55:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('message', message => {
+const filter = (reaction, user) => {
+    return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
+
+bot.on('message', function(message){
     if(message.content.toLowerCase().startsWith('smash or pass:'))
     {
+    var heartCount = 0;
+    var skullCount = 0;
         message.channel.send(
             "The vote begins! \n**Name**: smash or pass? Select :heart: to smash, :skull: to pass.")
                 .then(async function (message){
@@ -15,24 +20,28 @@ bot.on('message', message => {
                     console.error('One of the emojis failed to react.');
                     }
                 })
-		
-	    	const filter = (reaction, user) => {
-			return ['❤️', '💀'].includes(reaction.emoji.name);
-		};
-	    
-		var heartCount = 0;
-		var skullCount = 0;
-		
-		.then(awaitReactions(filter, {time: 5400})
-		.then(collected => {
-		    const reaction = collected.first();
-		    message.channel.send(reaction.emoji.name);
-            
-		}
-		));
+                .then(message.awaitReactions(filter, {time: 5400})
+                .then(collected => {
+                    for (var i = 0; i < collected.length; i++){
+                        if (collected[i].emoji.name === "❤️")
+                        {heartCount++;}
+                        else if (collected[i].emoji.name === "💀")
+                        {skullCount++;}
+                    };
+                
+                    if (heartCount > skullCount){
+                        message.channel.send("SMASH" + " " + heartCount + " " + skullCount);
+                    }
+                    else if (heartCount < skullCount){
+                        message.channel.send("PASS"+ " " + heartCount + " " + skullCount);
+                    }
+                    else {
+                        message.channel.send("TIE"+ " " + heartCount + " " + skullCount);
+                    }
+                    
+                })
+                )
     }
-    
-    
 });
 
 bot.login(process.env.BOT_TOKEN);
