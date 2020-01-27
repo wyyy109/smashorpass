28fa9a49a148ad14dd4e3e8538a9c47c7354ab3d
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-26 18:15:18 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const filter = (reaction, user) => {
+    return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id;
+    
+};
+
 bot.on('message', function(message){
     if(message.content.toLowerCase().startsWith('smash or pass:'))
     {
@@ -15,6 +20,29 @@ bot.on('message', function(message){
                     console.error('One of the emojis failed to react.');
                     }
                 })
+                .then(message.awaitReactions(filter, {time: 1800})
+                .then(collected => {
+                    var heartCount = 0;
+                    var skullCount = 0;
+                    for (var i = 0; i < collected.length; i++){
+                        if (collected[i].emoji.name === "❤️")
+                        {heartCount++;}
+                        else if (collected[i].emoji.name === "💀")
+                        {skullCount++;}
+                    };
+                
+                    if (heartCount > skullCount){
+                        message.channel.send("SMASH");
+                    }
+                    else if (heartCount < skullCount){
+                        message.channel.send("PASS");
+                    }
+                    else {
+                        message.channel.send("TIE");
+                    }
+                    
+                })
+                )
     }
 });
 
