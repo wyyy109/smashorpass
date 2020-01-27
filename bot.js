4300f9325d5943f242bbfb1c3f759b8d9fd38d85
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-26 22:38:52 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const filter = (reaction, user) => {
+    return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
+
 bot.on('message', function(message){
-    if(message.content.toLowerCase().startsWith('!vote'))
+    if(message.content.toLowerCase().startsWith('smash or pass:'))
     {
     var heartCount = 0;
     var skullCount = 0;
         message.channel.send(
-            //will later add way to change "Name" to fetched name from user message
-            "The vote begins! Do we love it or hate it?")
+            "The vote begins! \n**Name**: smash or pass? Select :heart: to smash, :skull: to pass.")
                 .then(async function (message){
                     try {
                     await message.react("❤️")
@@ -18,10 +20,7 @@ bot.on('message', function(message){
                     console.error('One of the emojis failed to react.');
                     }
                 })
-        const filter = (reaction, user) => {
-        return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
-                
-                message.awaitReactions(filter, {time: 10000})
+                .then(message.awaitReactions(filter, {time: 5400})
                 .then(collected => {
                     for (var i = 0; i < collected.length; i++){
                         if (collected[i].emoji.name === "❤️")
@@ -31,16 +30,17 @@ bot.on('message', function(message){
                     };
                 
                     if (heartCount > skullCount){
-                        message.channel.send("We love it!");
+                        message.channel.send("SMASH" + " " + heartCount + " " + skullCount);
                     }
                     else if (heartCount < skullCount){
-                        message.channel.send("We hate it.");
+                        message.channel.send("PASS"+ " " + heartCount + " " + skullCount);
                     }
                     else {
-                        message.channel.send("We're neutral about it.");
+                        message.channel.send("TIE"+ " " + heartCount + " " + skullCount);
                     }
                     
                 })
+                )
     }
 });
 
