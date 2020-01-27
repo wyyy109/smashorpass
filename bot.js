b61f4ded39a6c471cca51100b5399aa32e83a5a2
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-27 14:59:26 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,39 +5,8 @@ bot.on('message', function(message){
     var heartCount = 0;
     var skullCount = 0;
 
-    if(message.content.toLowerCase().match('smash or pass'))
-    {
-      message.channel.send("Smash or pass? \nSelect :heart: to smash, :skull: to pass.")
-      .then(msg => {
-        msg.react(`❤️`).then(() => msg.react('💀'));
-          const filter = (reaction, user) => {
-            return [`❤️`, '💀'].includes(reaction.emoji.name);
-            };
-
-            const collector = msg.createReactionCollector(filter, {time: 10000});
-            collector.on('collect', (reaction, reactionCollector) => {
-              if (reaction.emoji.name === `❤️`) {
-                heartCount+=1
-              } else if (reaction.emoji.name === `💀`) {
-                skullCount+=1
-                }
-            });
-            collector.on('end', (reaction, reactionCollector) => {
-              if (heartCount > skullCount){
-                message.channel.send("SMASH");
-              }
-              else if (heartCount < skullCount){
-                message.channel.send("Pass...");
-              }
-              else {
-                message.channel.send("Inconclusive.");
-              }
-            });
-        })
-    }
-
-    else if(message.content.toLowerCase().startsWith('smash or pass')) {
-      var toSmashTemp = message.content.slice(13);
+  if(message.content.toLowerCase().startsWith('smash or pass')) {
+      var toSmashTemp = message.content.toLowerCase().slice(13);
       var toSmash;
       if (toSmashTemp.charAt(0) === ":"){
         toSmash = toSmashTemp.slice(1).trim();
