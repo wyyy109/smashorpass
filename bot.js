29d22de5c500cd3e59964ad371fc492ce0e8a5a6
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-26 21:22:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,47 +1,41 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const filter = (reaction, user) => {
-    return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
-
-bot.on('message', function(message){
+bot.on('message', message => {
     if(message.content.toLowerCase().startsWith('smash or pass:'))
     {
-    var heartCount = 0;
-    var skullCount = 0;
         message.channel.send(
             "The vote begins! \n**Name**: smash or pass? Select :heart: to smash, :skull: to pass.")
-                .then(async function (message){
-                    try {
-                    await message.react("❤️")
-                    await message.react("💀")
-                    }
-                catch (error) {
-                    console.error('One of the emojis failed to react.');
-                    }
-                })
-                .then(message.awaitReactions(filter, {time: 5400})
-                .then(collected => {
-                    for (var i = 0; i < collected.length; i++){
+            .then(() => message.react('❤️'))
+            .then(() => message.react('💀'));
+            
+            const filter = (reaction, user) => {
+			return ['❤️', '💀'].includes(reaction.emoji.name) && user.id === message.author.id;
+		};
+		
+		var heartCount = 0;
+		var skullCount = 0;
+		
+		message.awaitReactions(filter, {time: 5400})
+		.then(collected => {
+		    for (var i = 0; i < collected.length; i++){
                         if (collected[i].emoji.name === "❤️")
                         {heartCount++;}
                         else if (collected[i].emoji.name === "💀")
                         {skullCount++;}
-                    };
-                
-                    if (heartCount > skullCount){
-                        message.channel.send("SMASH" + " " + heartCount + " " + skullCount);
                     }
-                    else if (heartCount < skullCount){
-                        message.channel.send("PASS"+ " " + heartCount + " " + skullCount);
-                    }
-                    else {
-                        message.channel.send("TIE"+ " " + heartCount + " " + skullCount);
-                    }
-                    
-                })
-                )
-    }
+            if (heartCount > skullCount){
+                message.channel.send("SMASH" + " " + heartCount + " " + skullCount);
+            }
+            else if (heartCount < skullCount){
+                message.channel.send("PASS"+ " " + heartCount + " " + skullCount);
+            }
+            else {
+                message.channel.send("TIE"+ " " + heartCount + " " + skullCount);
+            }
+		}
+		);
+    }  
 });
 
 bot.login(process.env.BOT_TOKEN);
