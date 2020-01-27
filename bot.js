cecd1394b8aed456d399adadab80dd525b6d53e0
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-27 13:02:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,39 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const filter = (reaction, user) => {
-    return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
-
 bot.on('message', function(message){
-    if(message.content.toLowerCase().startsWith('smash or pass:'))
-    var toSmash = message.content.slice(14).trim();
-    {
     var heartCount = 0;
     var skullCount = 0;
-        message.channel.send(
-            `**{$toSmash}**: smash or pass? \nSelect :heart: to smash, :skull: to pass. Vote ends in X time.`)
-                .then(async function (message){
-                    try {
-                    await message.react("❤️")
-                    await message.react("💀")
-                    }
-                catch (error) {
-                    console.error('One of the emojis failed to react.');
-                    }
-                })
-                .then(message.awaitReactions(filter, {time: 5400})
-                .then(collected => {
-                    for (var i = 0; i < collected.length; i++){
-                        if (collected[i].emoji.name === "❤️")
-                        {heartCount++;}
-                        else if (collected[i].emoji.name === "💀")
-                        {skullCount++;}
-                    };
-                
-                    if (heartCount > skullCount){
-                        message.channel.send(`Smash {$toSmash} by a vote of {$heartCount} to {$skullCount}.`);
+
+    if(message.content.toLowerCase().startsWith('!vote')) {
+        message.channel.send('The vote begins! Do we love it or hate it?').then(msg => {
+            msg.react(`❤️`).then(() => msg.react('💀'));
+            const filter = (reaction, user) => {
+                return [`❤️`, '💀'].includes(reaction.emoji.name);
+            };
+
+            const collector = msg.createReactionCollector(filter, {time: 10000});
+            collector.on('collect', (reaction, reactionCollector) => {
+                if (reaction.emoji.name === `❤️`) {
+                    heartCount+=1
+                } else if (reaction.emoji.name === `💀`) {
+                    skullCount+=1
+                }
+            });
+            collector.on('end', (reaction, reactionCollector) => {
+                   if (heartCount > skullCount){
+                        message.channel.send("We love it!");
                     }
                     else if (heartCount < skullCount){
-                        message.channel.send(`Pass on {$toSmash} by a vote of {$skullCount} to {$heartCount}.`);
+                        message.channel.send("We hate it.");
                     }
                     else {
-                        message.channel.send(`Inconclusive whether to smash {$toSmash}.`);
+                        message.channel.send("We're neutral about it.");
                     }
-                    
-                })
-                )
+            });
+
+        })
     }
-});
+})
 
 bot.login(process.env.BOT_TOKEN);
