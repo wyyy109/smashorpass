c7a45d7c91747b151005b19a96208cf1ce9e76c7
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-26 20:54:23 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,29 +4,44 @@ const bot = new Discord.Client();
 const filter = (reaction, user) => {
     return ["❤️","💀"].includes(reaction.emoji.name) && user.id === message.author.id };
 
-client.on('message', message => {
-	if (message.content === '!react-await') {
-		message.react('👍').then(() => message.react('👎'));
-
-		const filter = (reaction, user) => {
-			return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
-		};
-
-		message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-			.then(collected => {
-				const reaction = collected.first();
-
-				if (reaction.emoji.name === '👍') {
-					message.reply('you reacted with a thumbs up.');
-				} else {
-					message.reply('you reacted with a thumbs down.');
-				}
-			})
-			.catch(collected => {
-				console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-				message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
-			});
-	}
+bot.on('message', function(message){
+    if(message.content.toLowerCase().startsWith('smash or pass:'))
+    {
+    var heartCount = 0;
+    var skullCount = 0;
+        message.channel.send(
+            "The vote begins! \n**Name**: smash or pass? Select :heart: to smash, :skull: to pass.")
+                .then(async function (message){
+                    try {
+                    await message.react("❤️")
+                    await message.react("💀")
+                    }
+                catch (error) {
+                    console.error('One of the emojis failed to react.');
+                    }
+                })
+                .then(message.awaitReactions(filter, {time: 5400})
+                .then(collected => {
+                    for (var i = 0; i < collected.length; i++){
+                        if (collected[i].emoji.name === "❤️")
+                        {heartCount++;}
+                        else if (collected[i].emoji.name === "💀")
+                        {skullCount++;}
+                    };
+                
+                    if (heartCount > skullCount){
+                        message.channel.send("SMASH" + " " + heartCount + " " + skullCount);
+                    }
+                    else if (heartCount < skullCount){
+                        message.channel.send("PASS"+ " " + heartCount + " " + skullCount);
+                    }
+                    else {
+                        message.channel.send("TIE"+ " " + heartCount + " " + skullCount);
+                    }
+                    
+                })
+                )
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
