2291baf8519924b585972b7339e4a22c3c93b6ed
wyyy109 <46662873+wyyy109@users.noreply.github.com>
2020-01-27 15:02:39 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ bot.on('message', function(message){
     var heartCount = 0;
     var skullCount = 0;
 
-  if(message.content.toLowerCase().startsWith('smash or pass')) {
-      var toSmashTemp = message.content.toLowerCase().slice(13);
+    if(message.content.toLowerCase().startsWith('smash or pass')) {
+      var toSmashTemp = message.content.slice(13);
       var toSmash;
       if (toSmashTemp.charAt(0) === ":"){
         toSmash = toSmashTemp.slice(1).trim();
@@ -14,6 +14,8 @@ bot.on('message', function(message){
       else {
        toSmash = toSmashTemp.trim();
       }
+
+      if (toSmash.length > 0){
       message.channel.send(`**${toSmash}**: smash or pass? \nSelect :heart: to smash, :skull: to pass.`)
       .then(msg => {
         msg.react(`❤️`).then(() => msg.react('💀'));
@@ -40,7 +42,38 @@ bot.on('message', function(message){
                 message.channel.send("Inconclusive.");
               }
             });
-        })     
+        })
+      }
+      else {
+      message.channel.send("Vote now: mash or pass? \nSelect :heart: to smash, :skull: to pass.")
+      .then(msg => {
+        msg.react(`❤️`).then(() => msg.react('💀'));
+          const filter = (reaction, user) => {
+            return [`❤️`, '💀'].includes(reaction.emoji.name);
+            };
+
+            const collector2 = msg.createReactionCollector(filter, {time: 10000});
+            collector2.on('collect', (reaction, reactionCollector) => {
+              if (reaction.emoji.name === `❤️`) {
+                heartCount+=1
+              } else if (reaction.emoji.name === `💀`) {
+                skullCount+=1
+                }
+            });
+            collector2.on('end', (reaction, reactionCollector) => {
+              if (heartCount > skullCount){
+                message.channel.send("SMASH");
+              }
+              else if (heartCount < skullCount){
+                message.channel.send("Pass...");
+              }
+              else {
+                message.channel.send("Inconclusive.");
+              }
+            });
+        })
+      }
+      
     }
 })
 
